9d61df01df3261fd5395c2d850a55c1345951510
Stephen Blott <stephen.blott@dcu.ie>
2022-11-03 12:50:46 +0000

    Update verifier script for 2023.

--- ca282-merge-rebase-bisect--verify.zsh
+++ ca282-merge-rebase-bisect--verify.zsh
@@ -1,82 +1,101 @@
 #!/usr/bin/zsh
 
-python="python2.7"
+user=$argv[1]
 
-# If you want to use this script, then you probably want to copy
-# it to somewhere outside of the reop.
-
-# Usage... either:
-#
-#    $ zsh ../ca282-merge-rebase-bisect--verify.zsh
-#    (examine the repo in the current working directory)
-#
-# or:
-#
-#    $ zsh ../ca282-merge-rebase-bisect--verify.zsh DIRECTORY
-#    (examine the repo in DIRECTORY)
-
-integer points=0
-
-atexit () {
-   print $points
+mark () {
+  if [[ $#argv == 0 ]]
+  then
+    print "#mark" $user 1
+  else
+    print "#mark" $user $argv[1]
+  fi
 }
-trap atexit EXIT
 
-[[ $#argv == 0 ]] && set -- "."
-cd $argv[1]
+no_pyc () {
+  for pyc in *.pyc(.N)
+  do
+    print $pyc
+    mark -1
+  done
+}
 
-if [[ -n $( git status -s ) ]]
+# ######################################################
+# task 1
+
+if git merge-base --is-ancestor origin/subtract master
 then
-   print "error: you have uncommitted work"
-   print "       refusing to run"
-   exit
-fi >&2
+  mark
+fi
 
-# Checkout the various branches.
-set -e
-for branch in subtract multiply divide square master
-do
-   git checkout $branch
-done
-set +e
+if git merge-base --is-ancestor origin/multiply master
+then
+  mark
+fi
 
-# Verify that both subtract and multiply have been merged.
-#
-git merge-base --is-ancestor subtract master && points+=1
-git merge-base --is-ancestor multiply master && points+=1
+no_pyc
 
-# Poor-man's test of the various functions.
-#
-git checkout -q master
-$python -c "import arithmetic; print arithmetic.add(8, 9)" | grep -q -w 17
-$python -c "import arithmetic; print arithmetic.subtract(18, 5)" | grep -q -w 13 && points+=1
-$python -c "import arithmetic; print arithmetic.multiply(5, 7)" | grep -q -w 35 && points+=1
-[[ -f arithmetic.pyc ]] && rm arithmetic.pyc
-
-# Verify that master is now an ancestor of divide.
-#
-git checkout -q divide
-git merge-base --is-ancestor master divide && points+=3
-
-# Poor-man's test of the various functions.
-#
-$python -c "import arithmetic; print arithmetic.add(8, 9)" | grep -q -w 17
-$python -c "import arithmetic; print arithmetic.subtract(18, 5)" | grep -q -w 13
-$python -c "import arithmetic; print arithmetic.multiply(5, 7)" | grep -q -w 35
-$python -c "import arithmetic; print arithmetic.divide(45, 5)" | grep -q -w 9 && points+=1
-[[ -f arithmetic.pyc ]] && rm arithmetic.pyc
-
-git checkout -q square
-git merge-base --is-ancestor 58f763c6c98476f4bdc890372c5c3b1ff41083f2 square && points+=2
-
-# Poor-man's test of the various functions.
-#
-$python -c "import arithmetic; print arithmetic.add(8, 9)" | grep -q -w 17
-$python -c "import arithmetic; print arithmetic.square(12)" | grep -q -w 144 && points+=2
-[[ -f arithmetic.pyc ]] && rm arithmetic.pyc
-
-cat <<EOF
-Your repo will also be manually reviewed for issues which this script isn't
-able to detect.
-
+test_python_1 ()
+{
+  python2 <<EOF
+import arithmetic
+print arithmetic.subtract(101, 2)
+print arithmetic.multiply(11, 9)
 EOF
+}
+
+test_python_1 | grep -w 99 | wc -l | read count
+mark $count
+
+# ######################################################
+# task 2
+
+test_python_2 ()
+{
+  python2 <<EOF
+import arithmetic
+print arithmetic.divide(81, 9)
+EOF
+}
+
+if git merge-base --is-ancestor master origin/divide
+then
+  mark 1
+  #
+  find . -type f -name '*.pyc' -delete
+  if git checkout --quiet --track origin/divide
+  then
+    no_pyc
+
+    if test_python_2 | grep -q -w 9
+    then
+      mark 4
+    fi
+  fi
+fi
+
+# ######################################################
+# task 3
+
+test_python_3 ()
+{
+  python2 <<EOF
+import arithmetic
+print arithmetic.square(20)
+EOF
+}
+
+find . -type f -name '*.pyc' -delete
+if git checkout --quiet --track origin/square
+then
+  no_pyc
+
+  if test_python_3 | grep -q -w 400
+  then
+    mark 2
+  fi
+
+  if git log | grep -q "reverts.*fd8fbca9ca6ba859038f4c2e32d5fac0b0e2e743"
+  then
+    mark 2
+  fi
+fi
